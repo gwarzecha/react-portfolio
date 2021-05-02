d8893fb312263428caae2b72c1c8f61cb7bdb84b
gwarz <gmwarzecha@tutanota.com>
2021-05-02 13:12:08 -0700

    got link-containing texts to lie over the project imgs, added hover effects

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,10 +4,12 @@ import { GitHub } from 'react-feather';
 
 const Project = ({ project }) => {
   return (
-    <div className='col-sm' key = {project.id}>
+    <div className='col-sm  d-flex justify-content-center img-container' key={project.id}>
       <img src={require(`../assets/images/${project.image}`).default} />
-      <a href={project.deployedApp} target="_blank" role="button">{project.name}</a>
-      <a href={project.github} target="_blank" role="button"><GitHub color="black" size={32} /></a>
+      <span className='overlay d-flex justify-content-center'>
+        <a href={project.deployedApp} className=' appLink align-self-center p-2' target="_blank" role="button">{project.name}</a>
+        <a href={project.github} className='align-self-center p-2' target="_blank" role="button"><GitHub color="black" size={32} /></a>
+      </span>
     </div>
   )
 }
