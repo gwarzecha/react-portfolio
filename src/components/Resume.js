90534f92a52852a191a1aabbf9b42f8004ba72a2
gwarz <gmwarzecha@tutanota.com>
2021-05-02 21:50:23 -0700

    formatted resume page and added styling

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,34 +2,47 @@ import React from 'react'
 
 const Resume = () => {
   return (
-    <div>
-      <h1>View my <a href='https://docs.google.com/document/d/1QxQsZqhZsIMi8LB4Ajbp6Lp517oKCHD9LnrOWBbfENs/edit?usp=sharing'>resume</a></h1>
-      <h2>Skills and Proficiencies</h2>
-      <div>
-        <h3>Back-End Development</h3>
-        <ul>
-          <li>Node.js</li>
-          <li>Express.js</li>
-          <li>MongoDB</li>
-          <li>IndexedDB</li>
-          <li>NoSQL</li>
-          <li>MySQL</li>
-          <li>Sequelize</li>
-        </ul>
+    <section className='container resumeCont d-flex justify-content-center'>
+      <div className='row'>
+        <div className='col-12 d-flex justify-content-center'>
+          <h1>View my <a href='https://docs.google.com/document/d/1QxQsZqhZsIMi8LB4Ajbp6Lp517oKCHD9LnrOWBbfENs/edit?usp=sharing' className='resume'>resume</a></h1>
+        </div>
+
+        <div className='col-12 d-flex justify-content-center'>
+          <h2>Skills and Proficiencies</h2>
+        </div>
+     
+          <h3 className='col-12 d-flex justify-content-center'>Back-End Development</h3>
+          <div className='col-12 d-flex justify-content-center'>
+            <ul>
+              <li>Node.js</li>
+              <li>Express.js</li>
+              <li>MongoDB</li>
+              <li>IndexedDB</li>
+              <li>NoSQL</li>
+              <li>MySQL</li>
+              <li>Sequelize</li>
+            </ul>
+          </div>
+          <h3 className='col-12 d-flex justify-content-center'>Front-end Development</h3>
+          <div className='col-12 d-flex justify-content-center'>
+            <ul>
+              <li>JavaScript</li>
+              <li>React.js</li>
+              <li>CSS</li>
+              <li>Bootstrap</li>
+              <li>Tailwind.css</li>
+              <li>Materialize</li>
+              <li>jQuery</li>
+              <li>HTML</li>
+              <li>PWAs</li>
+            </ul>
+          </div>
+        
+
       </div>
-      <div>
-        <h3>Front-end Development</h3>
-        <ul>
-          <li>JavaScript</li>
-          <li>React.js</li>
-          <li>CSS (Bootstrap, Tailwind.css, Materialize)</li>
-          <li>jQuery</li>
-          <li>HTML</li>
-          <li>Responsive Web Development</li>
-          <li>PWAs</li>
-        </ul>
-      </div>
-    </div>
+
+    </section>
   )
 }
 
