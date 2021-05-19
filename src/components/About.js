6ff443b3f1031d9914a3405cda06f0dcc06bcf61
gwarz <gmwarzecha@tutanota.com>
2021-05-18 23:15:49 -0700

    updated About, Contact, Nav, and Resume page, mainly styling

--- src/components/About.js
+++ src/components/About.js
@@ -10,21 +10,14 @@ const About = () => {
         </div>
         <div className="col-md-12 col-sm-12 col-lg-8 d-flex p-2 mt-4">
           <p className="d-flex align-self-center secondaryFont">
-            I am a full-stack web developer from PHX, AZ. Web development
-            provides an outlet for combinging my creative passions with
-            practical problem solving abilities. Initially finding myself in the
-            medical field with a background in Biomedical Sciences, a transition
-            in focus to web development became the spark to realizing my
-            potential as an individual. As a problem-solver with strong
-            technical skills, accustomed to thriving in a highly collaborative
-            and ever-evolving environment, I am working harder each day towards
-            setting the bar higher, venturing outside of my comfort zone, and
-            building upon a steadily growing foundation of skill and team
-            collaboration.
-            <br></br>
-            <br></br>
-            In my freetime I enjoy collecting vinyl, reading, drawing, being
-            outdoors, and hanging out with my wife and two cats.
+            I am a Full-Stack Developer from PHX, AZ. Initially finding myself
+            in the medical field with a background in Biomedical Sciences, a
+            transition in focus to web development became the spark to realizing
+            my potential as an individual. I've built responsive web
+            applications using JavaScript, React, Node.js, MongoDB, and Express.
+            I am working harder each day towards setting the bar higher by
+            venturing outside of my comfort zone to build upon a steadily
+            growing foundation of skill and team collaboration.
           </p>
         </div>
       </div>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,27 +1,44 @@
-import React from 'react';
-import { Mail, Linkedin, GitHub } from 'react-feather';
-
+import React from "react";
+import { Mail, Linkedin, GitHub } from "react-feather";
 
 function ContactForm() {
-
   return (
-    <section className="container d-flex contactCont justify-content-center" >
-      <div className='row'>
-        <div className='col-12 p-4'>
-          <h1 className='d-flex mainFont'>Get in Touch!</h1>
+    <section className="container d-flex contactCont justify-content-center">
+      <div className="row">
+        <div className="col-12 p-4">
+          <h1 className="d-flex mainFont justify-content-center">
+            Get in Touch!
+          </h1>
         </div>
-        <div className='col-12 justify-content-center p-4'>
-          <p className='mainFont contactInfo'>Phone:
-            <a href='tel:480-321-7571' rel='nofeferrer' target='blank' className='contactValue mainFont'> 480.321.7571</a>
+        <div className="col-12 p-4">
+          <p className="d-flex justify-content-center contactInfo">
+            <a
+              href="tel:480-321-7571"
+              rel="nofeferrer"
+              target="blank"
+              className="contactValue mainFont"
+            >
+              {" "}
+              480.321.7571
+            </a>
           </p>
-          <p className='mainFont contactInfo'>Email:
-            <a href="mailto: gmwarzecha@gmail.com" target="_blank" rel="noreferrer" className='contactValue mainFont'> gmwarzecha@gmail.com</a>
+        </div>
+        <div className="col-12 p-4">
+          <p className="d-flex justify-content-center contactInfo">
+            <a
+              href="mailto: gmwarzecha@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+              className="contactValue mainFont"
+            >
+              {" "}
+              gmwarzecha@gmail.com
+            </a>
           </p>
         </div>
       </div>
     </section>
   );
-
 }
 
 export default ContactForm;
