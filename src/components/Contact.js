d776ea3860192efee8d4b6bb0bc1987d231f0383
gwarz <gmwarzecha@tutanota.com>
2021-05-18 20:48:59 -0700

    updated contact form a bit more

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Mail, Linkedin, GitHub } from 'react-feather';
 
 
 function ContactForm() {
@@ -10,18 +11,18 @@ function ContactForm() {
           <h1 className='d-flex justify-content-center mainFont'>Get in Touch!</h1>
         </div>
         <div className='col-12 justify-content-center'>
-          <p>Phone:
-            <a href='tel:480-321-7571' rel='nofeferrer' target='blank'> 480.321.7571</a>
+          <p className='mainFont contactInfo'>Phone:
+            <a href='tel:480-321-7571' rel='nofeferrer' target='blank' className='mainFont'> 480.321.7571</a>
           </p>
-          <p>Email:
-            <a href="mailto: gmwarzecha@gmail.com" target="_blank"> gmwarzecha@gmail.com</a>
+          <p className='mainFont'>Email:
+            <a href="mailto: gmwarzecha@gmail.com" target="_blank" className='mainFont'> gmwarzecha@gmail.com</a>
           </p>
-          <p>LinkedIn:
+          <p className='mainFont'>LinkedIn:
           <a href="https://linkedin.com/in/garrett-warzecha-878906154" 
-                target="_blank"> LinkedIn</a>
+                target="_blank"> <Linkedin color="#034078ff" size={32} /></a>
           </p>
-          <p>GitHub:
-          <a href="https://github.com/gwarzecha" target="_blank"> GitHub</a>
+          <p className='mainFont'>GitHub:
+          <a href="https://github.com/gwarzecha" target="_blank"> <GitHub color="#034078ff" size={32} /></a>
           </p>
         </div>
       </div>
