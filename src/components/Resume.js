1a02e6ee80c38dc2be1098e1ba46a2012a5dc2f5
gwarz <gmwarzecha@tutanota.com>
2021-06-02 22:40:32 -0700

    added devicons in lieu of text for skills, still needs formatting and working URLs

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,44 +1,185 @@
-import React from 'react'
+import React from "react";
 
 const Resume = () => {
   return (
-    <section className='container resumeCont d-flex justify-content-center'>
-      <div className='row'>
-        <div className='col-12 d-flex justify-content-center'>
-          <h2 className='mainFont pb-4'><a href='https://docs.google.com/document/d/1fjfslIhiA87JXA_jZkCM74_DHdbwqm7DfytbZshHtFc/edit?usp=sharing' className='resume mainFont' target='_blank' rel="noreferrer" >Click to view my resume</a></h2>
+    <section className="container resumeCont d-flex justify-content-center">
+      <div className="row">
+        <div className="col-12 d-flex justify-content-center">
+          <h2 className="mainFont pb-4">
+            <a
+              href="https://docs.google.com/document/d/1fjfslIhiA87JXA_jZkCM74_DHdbwqm7DfytbZshHtFc/edit?usp=sharing"
+              className="resume mainFont"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Click to view my resume
+            </a>
+          </h2>
         </div>
-        <div className='col-12 d-flex justify-content-center'>
-          <h3 className='mainFont pb-2'>Skills and Proficiencies</h3>
-        </div>   
-          <h4 className='col-12 d-flex justify-content-center secondaryFont'>Back-End Development</h4>
-          <div className='col-12 d-flex justify-content-center secondaryFont'>
-            <ul>
-              <li>Node.js</li>
-              <li>Express.js</li>
-              <li>MongoDB</li>
-              <li>IndexedDB</li>
-              <li>NoSQL</li>
-              <li>MySQL</li>
-              <li>Sequelize</li>
-            </ul>
+        <div className="col-12 d-flex justify-content-center">
+          <h3 className="mainFont pb-2">Skills and Proficiencies</h3>
+        </div>
+        <div className="col-12 d-flex secondaryFont">
+          <div>
+            {" "}
+            <a
+              class="devicon-link"
+              href="https://devcenter.heroku.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div>
+                <i class="devicon devicon-nodejs-plain-wordmark colored"></i>
+              </div>
+            </a>
+            <a
+              class="devicon-link"
+              href="https://devcenter.heroku.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div>
+                <i class="devicon devicon-heroku-plain-wordmark colored"></i>
+              </div>
+            </a>
+            <a
+              class="devicon-link"
+              href="https://devcenter.heroku.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div>
+                <i class="devicon devicon-express-original-wordmark colored"></i>
+              </div>
+            </a>
+            <a
+              class="devicon-link"
+              href="https://devcenter.heroku.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div>
+                <i class="devicon devicon-mongodb-plain-wordmark colored"></i>
+              </div>
+            </a>
+            <a
+              class="devicon-link"
+              href="https://devcenter.heroku.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div>
+                <i class="devicon devicon-npm-original-wordmark colored"></i>
+              </div>
+            </a>
+            <a
+              class="devicon-link"
+              href="https://devcenter.heroku.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div>
+                <i class="devicon devicon-git-plain-wordmark colored"></i>
+              </div>
+            </a>
+            <a
+              class="devicon-link"
+              href="https://devcenter.heroku.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div>
+                <i class="devicon devicon-visualstudio-plain-wordmark colored"></i>
+              </div>
+            </a>
+            <a
+              class="devicon-link"
+              href="https://devcenter.heroku.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div>
+                <i class="devicon devicon-mysql-plain-wordmark colored"></i>
+              </div>
+            </a>
+            <a
+              class="devicon-link"
+              href="https://devcenter.heroku.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div>
+                <i class="devicon devicon-handlebars-plain-wordmark colored"></i>
+              </div>
+            </a>
+            <a
+              class="devicon-link"
+              href="https://devcenter.heroku.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div>
+                <i class="devicon devicon-sequelize-plain-wordmark colored"></i>
+              </div>
+            </a>
           </div>
-          <h4 className='col-12 d-flex justify-content-center secondaryFont'>Front-end Development</h4>
-          <div className='col-12 d-flex justify-content-center secondaryFont'>
-            <ul>
-              <li>JavaScript</li>
-              <li>React.js</li>
-              <li>CSS</li>
-              <li>Bootstrap</li>
-              <li>Tailwind.css</li>
-              <li>Materialize</li>
-              <li>jQuery</li>
-              <li>HTML</li>
-              <li>PWAs</li>
-            </ul>
+
+          <div>
+            <a
+              class="devicon-link"
+              href="https://devcenter.heroku.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div>
+                <i class="devicon devicon-react-plain-wordmark colored"></i>
+              </div>
+            </a>
+            <a
+              class="devicon-link"
+              href="https://devcenter.heroku.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div>
+                <i class="devicon devicon-css3-plain-wordmark colored"></i>
+              </div>
+            </a>
+            <a
+              class="devicon-link"
+              href="https://devcenter.heroku.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div>
+                <i class="devicon devicon-bootstrap-plain-wordmark colored"></i>
+              </div>
+            </a>
+            <a
+              class="devicon-link"
+              href="https://devcenter.heroku.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div>
+                <i class="devicon devicon-jquery-plain-wordmark colored"></i>
+              </div>
+            </a>
+            <a
+              class="devicon-link"
+              href="https://devcenter.heroku.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div>
+                <i class="devicon devicon-html5-plain-wordmark colored"></i>
+              </div>
+            </a>
           </div>
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Resume
+export default Resume;
