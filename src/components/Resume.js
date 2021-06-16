f4b0779eeadea87fa3ea30390f3e1d260453cf37
gwarz <gmwarzecha@tutanota.com>
2021-06-15 19:49:48 -0700

    got icons on the resume page to align properly and achieved correct size

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -17,165 +17,161 @@ const Resume = () => {
           </h2>
         </div>
         <div className="col-12 d-flex justify-content-center">
-          <h3 className="mainFont pb-2">Skills and Proficiencies</h3>
+          <h3 className="mainFont pb-2 m-2">Skills and Proficiencies</h3>
         </div>
-        <div className="col-12 d-flex secondaryFont">
-          <div>
-            {" "}
-            <a
-              class="devicon-link"
-              href="https://devcenter.heroku.com/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div>
-                <i class="devicon devicon-nodejs-plain-wordmark colored"></i>
-              </div>
-            </a>
-            <a
-              class="devicon-link"
-              href="https://devcenter.heroku.com/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div>
-                <i class="devicon devicon-heroku-plain-wordmark colored"></i>
-              </div>
-            </a>
-            <a
-              class="devicon-link"
-              href="https://devcenter.heroku.com/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div>
-                <i class="devicon devicon-express-original-wordmark colored"></i>
-              </div>
-            </a>
-            <a
-              class="devicon-link"
-              href="https://devcenter.heroku.com/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div>
-                <i class="devicon devicon-mongodb-plain-wordmark colored"></i>
-              </div>
-            </a>
-            <a
-              class="devicon-link"
-              href="https://devcenter.heroku.com/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div>
-                <i class="devicon devicon-npm-original-wordmark colored"></i>
-              </div>
-            </a>
-            <a
-              class="devicon-link"
-              href="https://devcenter.heroku.com/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div>
-                <i class="devicon devicon-git-plain-wordmark colored"></i>
-              </div>
-            </a>
-            <a
-              class="devicon-link"
-              href="https://devcenter.heroku.com/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div>
-                <i class="devicon devicon-visualstudio-plain-wordmark colored"></i>
-              </div>
-            </a>
-            <a
-              class="devicon-link"
-              href="https://devcenter.heroku.com/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div>
-                <i class="devicon devicon-mysql-plain-wordmark colored"></i>
-              </div>
-            </a>
-            <a
-              class="devicon-link"
-              href="https://devcenter.heroku.com/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div>
-                <i class="devicon devicon-handlebars-plain-wordmark colored"></i>
-              </div>
-            </a>
-            <a
-              class="devicon-link"
-              href="https://devcenter.heroku.com/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div>
-                <i class="devicon devicon-sequelize-plain-wordmark colored"></i>
-              </div>
-            </a>
-          </div>
 
-          <div>
-            <a
-              class="devicon-link"
-              href="https://devcenter.heroku.com/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div>
-                <i class="devicon devicon-react-plain-wordmark colored"></i>
-              </div>
-            </a>
-            <a
-              class="devicon-link"
-              href="https://devcenter.heroku.com/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div>
-                <i class="devicon devicon-css3-plain-wordmark colored"></i>
-              </div>
-            </a>
-            <a
-              class="devicon-link"
-              href="https://devcenter.heroku.com/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div>
-                <i class="devicon devicon-bootstrap-plain-wordmark colored"></i>
-              </div>
-            </a>
-            <a
-              class="devicon-link"
-              href="https://devcenter.heroku.com/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div>
-                <i class="devicon devicon-jquery-plain-wordmark colored"></i>
-              </div>
-            </a>
-            <a
-              class="devicon-link"
-              href="https://devcenter.heroku.com/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div>
-                <i class="devicon devicon-html5-plain-wordmark colored"></i>
-              </div>
-            </a>
-          </div>
+        <div className="row d-flex flex-direction-row flex-wrap justify-content-center m-4">
+          {" "}
+          <a
+            className="devicon-link"
+            href="https://devcenter.heroku.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="col-sm-4 col-md-2 p-3">
+              <i className="devicon devicon-nodejs-plain-wordmark colored"></i>
+            </div>
+          </a>
+          <a
+            className="devicon-link"
+            href="https://devcenter.heroku.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="col-sm-4 col-md-2 p-3">
+              <i className="devicon devicon-heroku-plain-wordmark colored"></i>
+            </div>
+          </a>
+          <a
+            className="devicon-link"
+            href="https://devcenter.heroku.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="col-sm-4 col-md-2 p-3">
+              <i className="devicon devicon-express-original-wordmark colored"></i>
+            </div>
+          </a>
+          <a
+            className="devicon-link"
+            href="https://devcenter.heroku.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="col-sm-4 col-md-2 p-3">
+              <i className="devicon devicon-mongodb-plain-wordmark colored"></i>
+            </div>
+          </a>
+          <a
+            className="devicon-link"
+            href="https://devcenter.heroku.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="col-sm-4 col-md-2 p-3">
+              <i className="devicon devicon-npm-original-wordmark colored"></i>
+            </div>
+          </a>
+          <a
+            className="devicon-link"
+            href="https://devcenter.heroku.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="col-sm-4 col-md-2 p-3">
+              <i className="devicon devicon-git-plain-wordmark colored"></i>
+            </div>
+          </a>
+          <a
+            className="devicon-link"
+            href="https://devcenter.heroku.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="col-sm-4 col-md-2 p-3">
+              <i className="devicon devicon-visualstudio-plain-wordmark colored"></i>
+            </div>
+          </a>
+          <a
+            className="devicon-link"
+            href="https://devcenter.heroku.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="col-sm-4 col-md-2 p-3">
+              <i className="devicon devicon-mysql-plain-wordmark colored"></i>
+            </div>
+          </a>
+          <a
+            className="devicon-link"
+            href="https://devcenter.heroku.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="col-sm-4 col-md-2 p-3">
+              <i className="devicon devicon-handlebars-plain-wordmark colored"></i>
+            </div>
+          </a>
+          <a
+            className="devicon-link"
+            href="https://devcenter.heroku.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="col-sm-4 col-md-2 p-3">
+              <i className="devicon devicon-sequelize-plain-wordmark colored"></i>
+            </div>
+          </a>
+          <a
+            className="devicon-link"
+            href="https://devcenter.heroku.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="col-sm-4 col-md-2 p-3">
+              <i className="devicon devicon-react-plain-wordmark colored"></i>
+            </div>
+          </a>
+          <a
+            className="devicon-link"
+            href="https://devcenter.heroku.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="col-sm-4 col-md-2 p-3">
+              <i className="devicon devicon-css3-plain-wordmark colored"></i>
+            </div>
+          </a>
+          <a
+            className="devicon-link"
+            href="https://devcenter.heroku.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="col-sm-4 col-md-2 p-3">
+              <i className="devicon devicon-bootstrap-plain-wordmark colored"></i>
+            </div>
+          </a>
+          <a
+            className="devicon-link"
+            href="https://devcenter.heroku.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="col-sm-4 col-md-2 p-3">
+              <i className="devicon devicon-jquery-plain-wordmark colored"></i>
+            </div>
+          </a>
+          <a
+            className="devicon-link"
+            href="https://devcenter.heroku.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="col-sm-4 col-md-2 p-3">
+              <i className="devicon devicon-html5-plain-wordmark colored"></i>
+            </div>
+          </a>
         </div>
       </div>
     </section>
