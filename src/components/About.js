2d95e4bb5844a112c5f1956312d51adf3137f5e7
gwarz <gmwarzecha@tutanota.com>
2022-04-11 11:18:31 -0700

    remove one of the products cuz someone hijacked the deployment url

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,8 @@ const About = () => {
         </div>
         <div className="col-md-12 col-sm-12 col-lg-8 d-flex p-2 mt-4">
           <p className="d-flex align-self-center secondaryFont">
-            I am a Full-Stack Developer from PHX, AZ. I am currently a Technical
+            I am a Full-Stack st animations and 
+            Developer from PHX, AZ. I am currently a Technical
             Support Specialist for ClickUp, providing client support and
             solutions for various bugs and other issues they encounter. I've
             built responsive web applications using JavaScript and technologies
